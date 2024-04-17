6540b20dbfd3b37fe1269b55a5f64d90fc51586d
Ritesh <13singhritesh@gmail.com>
2024-04-17 16:39:21 +0530

    Navbar Added

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Bussiness from './Bussiness';
+import Billing from './Billing';
+import Stats from './Stats';
+import CardDeals from './CardDeals';
+import Testimonials from './Testimonials';
+import Clients from './Clients';
+import CTA from './CTA';
+import Footer from './Footer'
+
+export {
+    Navbar,
+    Hero,
+    Bussiness,
+    Billing,
+    Stats,
+    CardDeals,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer
+};
